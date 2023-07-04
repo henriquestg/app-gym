e4afdb1eda80ac20d43a77966208449bb36558f0
Henrique <rigoni1912@gmail.com>
2023-07-03 21:20:45 -0300

    add new interface

---
+++ app-gym/src/components/HomeHeader.tsx
@@ -0,0 +1,40 @@
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacity } from "react-native";
+
+
+
+export function HomeHeader() {
+  return (
+    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto
+        source={{ uri: 'https://github.com/henriquestg.png' }}
+        alt="Imagem do usuario"
+        size={16}
+        mr={4}
+
+      />
+      <VStack flex={1}>
+        <Text color='gray.200'>
+          Ola,
+        </Text>
+
+        <Heading color='gray.200'>
+          Henrique
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout"
+          color='gray.200'
+          size={7}
+        />
+      </TouchableOpacity>
+    </HStack>
+
+  );
+}
--- app-gym/src/screens/Home.tsx
+++ app-gym/src/screens/Home.tsx
@@ -1,10 +1,18 @@
-import { Center, Text } from 'native-base'
+import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
+import { Center, HStack, Text, VStack } from 'native-base'
 
 
 export function Home() {
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <HStack>
+        <Group name="costa" />
+        <Group name="ombro" />
+      </HStack>
+
+    </VStack>
   )
 }
