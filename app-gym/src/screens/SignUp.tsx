d45793c9b2a37322cb6611dbb278b81a35f1b0f2
Henrique <rigoni1912@gmail.com>
2023-07-28 20:56:26 -0300

    implementation of axios mode

--- app-gym/src/screens/SignUp.tsx
+++ app-gym/src/screens/SignUp.tsx
@@ -1,4 +1,13 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
+
 import { useForm, Controller } from "react-hook-form";
 
 import LogoSvg from "@assets/logo.svg";
@@ -6,10 +15,15 @@ import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
 import { useNavigation } from "@react-navigation/native";
 
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import { api } from "@services/api";
+
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -32,6 +46,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const toast = useToast();
+
   const {
     control,
     handleSubmit,
@@ -47,16 +63,21 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch("http://192.168.0.108:3333/users", {
-      method: "POST",
-      headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
